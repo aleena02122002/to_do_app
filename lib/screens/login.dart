e99ed0944e5729745db7531abb912cf72fdf4363
aleena02122002 <khananna919@gmail.com>
2024-04-28 14:02:45 +0500

    to_do_application

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,6 +9,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  FocusNode _focusNode1 = FocusNode();
+  FocusNode _focusNode2 = FocusNode();
+
+  final email = TextEditingController();
+  final password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +21,51 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [const SizedBox(height: 20), image()],
+            children: [
+              const SizedBox(height: 20),
+              image(),
+              const SizedBox(height: 50),
+              textfield(email, _focusNode1, "Email", Icons.email),
+              const SizedBox(height: 10),
+              textfield(password, _focusNode2, "Password", Icons.lock)
+            ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget textfield(TextEditingController _controller, FocusNode _focusNode, String typeName, IconData _icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: TextField(
+          controller: _controller,
+          focusNode: _focusNode,
+          style: TextStyle(fontSize: 18, color: Colors.black),
+          decoration: InputDecoration(
+              prefixIcon: Icon(Icons.email,
+                  color:
+                      _focusNode.hasFocus ? costum_green : Color(0xffc5c5c5)),
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              hintText: typeName,
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Color(0xffc5c5c5),
+                    width: 2.0,
+                  )),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: costum_green,
+                    width: 2.0,
+                  ))),
         ),
       ),
     );
